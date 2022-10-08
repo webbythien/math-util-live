e69eb4ffd31da96d17f72421920839a7d409aeb5
webbythien <thiengk563@gmail.com>
2022-10-08 19:43:57 +0700

    Lan Dau Dua Code Len Server

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author thien
+ */
+public class MathUtil {
+
+    public static long getFactorial(int n) {
+         if(n<0 || n>20)
+             throw new IllegalArgumentException();
+         
+        if(n==0||n==1)
+            return 1;
+        long product =1;
+        for (int i = 2; i <=n; i++) {
+            product = product*i;
+        }
+        return product;
+    }
+}
